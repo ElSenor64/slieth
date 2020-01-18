1dcfbb818c44ebe888801b4127666bb2ac7b4c0a
ElSenor64 <sillynoodle36@gmail.com>
2020-01-18 10:42:15 -0700

    Changed git cred to updated keychain handlers.

--- gitcred.sh
+++ gitcred.sh
@@ -1,5 +1,13 @@
 # https://askubuntu.com/questions/740183/store-git-credentials-permanently-and-encrypted-using-a-keystore-in-ubuntu
 
-sudo apt-get install libgnome-keyring-dev
-sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring
-git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+# Bionic Only (Has been depricated)
+#sudo apt-get install libgnome-keyring-dev
+#sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring
+#git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+
+# All new versions (bionic+)
+# https://askubuntu.com/questions/773455/what-is-the-correct-way-to-use-git-with-gnome-keyring-and-https-repos
+sudo apt-get install libsecret-1-0 libsecret-1-dev
+cd /usr/share/doc/git/contrib/credential/libsecret
+sudo make
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
