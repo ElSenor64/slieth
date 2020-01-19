b1e15127bba7abafd71411b33fa587e6837e3afb
ElSenor64 <sillynoodle36@gmail.com>
2020-01-19 14:17:11 -0700

    Added gitcloneall.sh and git pullall.sh

---
+++ gitcloneall.sh
@@ -0,0 +1,9 @@
+cd Documents/
+git clone https://github.com/ElSenor64/slieth.git
+git clone https://github.com/ElSenor64/tuxtoy.git
+git clone https://github.com/ElSenor64/da-lern-place.git
+git clone https://github.com/trainsgreen/maze.git
+git clone https://github.com/th4t-gi/CJ-Gateway.git
+git clone https://github.com/Cougars-Gone-Wired/2019-FRC-Code.git
+git clone https://github.com/Cougars-Gone-Wired/2020-FRC-Code.git
+git clone https://github.com/Cougars-Gone-Wired/Color-Sensor-2020.git
---
+++ gitpullall.sh
@@ -0,0 +1,13 @@
+cd Documents/slieth
+git pull
+cd ../2020-FRC-Code
+git pull
+cd ../GitPractice2020
+git pull
+cd ../CJ-Gateway
+git pull
+cd ../da-lern-place
+git pull
+cd ../tuxtoy
+git pull
+exit
